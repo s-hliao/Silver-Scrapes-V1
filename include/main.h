ad0f5a2bc7dbd2ba68b8812c3a3388e17875cd26
georgezhang02 <georgezhang02@hotmail.com>
2020-06-30 23:05:37 -0700

    Clarified main.h includes

--- include/main.h
+++ include/main.h
@@ -4,17 +4,12 @@
 #define PROS_USE_SIMPLE_NAMES
 #define PROS_USE_LITERALS
 
-#include "pros/motors.h"
-#include "pros/apix.h"
-#include "okapi/api.hpp"
 #include "robotConfig.h"
 
 #include "drive.hpp"
 #include "rollers.hpp"
 #include "lift.hpp"
 
-
-//#include "okapi/api.hpp"
 //#include "pros/api_legacy.h"
 
 #ifdef __cplusplus
