42aaa348614ca1d79936615a5d0b382c48c51de4
Henry <henry.z.liao@hotmail.com>
2020-06-30 23:50:45 -0700

    Got Theo stuff in here

--- include/GUI.hpp
+++ include/GUI.hpp
@@ -20,12 +20,12 @@ class GUI{
 
       sensors_t(double ileft, double iright, double imiddle);
 
-      private:
+
         bool hasMiddle {false};
-        friend class OdomDebug;
     };
 
   GUI(lv_obj_t* parent);
+  GUI(lv_obj_t* parent, lv_color_t mainColor);
   ~GUI();
 
   void setStateCallback(std::function<void(state_t state)> callback);
@@ -33,5 +33,48 @@ class GUI{
   void setResetCallback(std::function<void()> callback);
 
   void setData(state_t state, sensors_t sensors);
+
+
+
+
+// odometry part
+  lv_obj_t* container = nullptr;
+  lv_style_t cStyle;
+
+   // field
+   lv_style_t fStyle;
+   double fieldDim = 0; // width and height of field container
+
+   // tile styles
+  lv_style_t grey;
+  lv_style_t red;
+  lv_style_t blue;
+
+  // robot point
+  lv_obj_t* led = nullptr;
+  lv_style_t ledStyle;
+
+  // robot line
+  lv_obj_t* line = nullptr;
+  lv_style_t lineStyle;
+  std::vector<lv_point_t> linePoints = {{0, 0}, {0, 0}}; // line positions
+  int lineWidth = 0;
+  int lineLength = 0;
+
+  // status label
+  lv_obj_t* statusLabel = nullptr;
+  lv_style_t textStyle;
+
+  // reset button styles
+  lv_style_t resetRel;
+  lv_style_t resetPr;
+
+  // external callbacks to interface with odometry
+  std::function<void(state_t state)> stateFnc = nullptr;
+  std::function<void()> resetFnc = nullptr;
+
+  static lv_res_t tileAction(lv_obj_t*); // action when tile is pressed
+  static lv_res_t resetAction(lv_obj_t*); // action when reset button is pressed
+
 
 };
