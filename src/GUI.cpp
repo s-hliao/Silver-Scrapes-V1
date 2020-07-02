32998e15f7d868873cee1a0be447ca03a3b5b7b0
georgezhang02 <georgezhang02@hotmail.com>
2020-07-02 00:40:50 -0700

    Button test

--- src/GUI.cpp
+++ src/GUI.cpp
@@ -3,12 +3,10 @@
 
 void GUIcreate(void){
 
-  th = lv_theme_mono_init(12, NULL);
+    th = lv_theme_mono_init(12, NULL);
 
-    lv_theme_set_current(lv_theme_get_mono()); //when removed Spinbox works fine
+    lv_theme_set_current(lv_theme_get_mono());
     tv = lv_tabview_create(lv_scr_act(), NULL);
-
-
 
     // Tabs
     t1 = lv_tabview_add_tab(tv, "Auto Select");
@@ -17,15 +15,42 @@ void GUIcreate(void){
     t4 = lv_tabview_add_tab(tv, "Temperature");
     t5 = lv_tabview_add_tab(tv, "Debug");
 
-/*    lv_style_init(&style_box);
-    lv_style_set_value_align(&style_box, LV_STATE_DEFAULT, LV_ALIGN_OUT_TOP_LEFT);
-    lv_style_set_value_ofs_y(&style_box, LV_STATE_DEFAULT, - LV_DPX(10));
-    lv_style_set_margin_top(&style_box, LV_STATE_DEFAULT, LV_DPX(30));
-
     // Create tabs
     auto_create(t1);
     odom_create(t2);
     pid_create(t3);
     temp_create(t4);
-    debug_create(t5);*/
+    debug_create(t5);
+}
+
+static void auto_create(lv_obj_t * parent)
+{
+  lv_page_set_scrl_layout(parent, LV_LAYOUT_PRETTY);
+
+  lv_obj_t * btn = lv_btn_create(parent, NULL);
+  lv_btn_set_fit(btn, 0, 1);
+  lv_obj_set_width(btn, 0);
+  lv_obj_t * label = lv_label_create(btn, NULL);
+  lv_label_set_text(label ,"Button");
+  
+}
+
+static void controls_create(lv_obj_t * parent)
+{
+  lv_page_set_scrl_layout(parent, LV_LAYOUT_PRETTY);
+}
+
+static void pid_create(lv_obj_t * parent)
+{
+  lv_page_set_scrl_layout(parent, LV_LAYOUT_PRETTY);
+}
+
+static void temp_create(lv_obj_t * parent)
+{
+  lv_page_set_scrl_layout(parent, LV_LAYOUT_PRETTY);
+}
+
+static void debug_create(lv_obj_t * parent)
+{
+  lv_page_set_scrl_layout(parent, LV_LAYOUT_PRETTY);
 }
