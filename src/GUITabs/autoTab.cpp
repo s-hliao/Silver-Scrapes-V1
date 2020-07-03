6a195e4a7f907dee34fc5f56562e44a9398a7a3f
georgezhang02 <georgezhang02@hotmail.com>
2020-07-02 22:26:55 -0700

    bug fix #3

--- src/GUITabs/autoTab.cpp
+++ src/GUITabs/autoTab.cpp
@@ -41,8 +41,8 @@ static lv_res_t left_click(lv_obj_t * btn){
   if (!lv_btn_get_state(btn1)) {
     autoSide = LEFT;
     lv_btn_set_state(btn2, false);
-  }
-  lv_btn_set_state(btn1, true);
+  } else
+    lv_btn_set_state(btn1, true);
   return LV_RES_OK;
 }
 
@@ -50,7 +50,7 @@ static lv_res_t right_click(lv_obj_t * btn){
   if (!lv_btn_get_state(btn2)) {
     autoSide = RIGHT;
     lv_btn_set_state(btn2, true);
-  }
-  lv_btn_set_state(btn1, false);
+  } else
+    lv_btn_set_state(btn1, false);
   return LV_RES_OK;
 }
