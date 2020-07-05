511d33cef62f48d1d51f55004cb282ea94b5a5ac
georgezhang02 <georgezhang02@hotmail.com>
2020-07-05 14:15:14 -0700

    Minor fix

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -17,5 +17,5 @@ void opcontrol(){
   	pros::Task driveT(driveOp, (void*)"PROS", TASK_PRIORITY_DEFAULT,TASK_STACK_DEPTH_DEFAULT, "Drive Control");
     pros::Task intakeT(intakeOp, (void*)"PROS", TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "Intake Control");
     pros::Task flywheelT(flywheelOp, (void*)"PROS", TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "Flywheel Control");
-    pros::Task debugT(debugOp, (void*)"PROS", TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "Debug");
+    //pros::Task debugT(debugOp, (void*)"PROS", TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "Debug");
 }
