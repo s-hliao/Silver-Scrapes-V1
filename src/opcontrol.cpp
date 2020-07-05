e95954953b6790d7329d0f9c4e574de5d1407fb3
Henry <henry.z.liao@hotmail.com>
2020-07-05 14:26:51 -0700

    Deleted flywheel.hpp

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -6,8 +6,4 @@ void opcontrol(){
   	pros::Task driveT(driveOp, (void*)"PROS", TASK_PRIORITY_DEFAULT,TASK_STACK_DEPTH_DEFAULT, "Drive Control");
     pros::Task intakeT(intakeOp, (void*)"PROS", TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "Intake Control");
     pros::Task flywheelT(flywheelOp, (void*)"PROS", TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "Flywheel Control");
-<<<<<<< HEAD
-=======
-    //pros::Task debugT(debugOp, (void*)"PROS", TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "Debug");
->>>>>>> 511d33cef62f48d1d51f55004cb282ea94b5a5ac
 }
